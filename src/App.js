8b80d3e3047fb82b2bfd3bfd66f897727274ed68
vkiran.jakkapu <vkiranjakkapu>
2020-04-03 12:46:35 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.ico';
 import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Package from './data.json';
+// import Details from './details';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component
+{
+  // constructor() {
+  //   super();
+  //   this.Data={
+  //     name: "Venkata Kiran",
+  //     age: 19
+  //   }
+  // }
+
+  // increase=()=>{
+  //   this.setState({
+  //     age:this.Data.age+1
+  //   })
+  // }
+  // decrease=()=>{
+  //   this.setState({
+  //     age:this.Data.age-1
+  //   })
+  // }
+  render() {
+    const pack = Package.data;
+    return (
+      <div>
+        {/* <BrowserRouter>
+          <Route exact path="/details" component={Det}></Route>
+        </BrowserRouter> */}
+        {pack.map((i,j)=>(
+          <div key={j}>{i.Details.name}</div>
+        ))}
+      </div>
+    )
+  }
 }
+
+// let Det=()=>{
+//   return(
+//     <Details></Details>
+//   )
+// }
 
 export default App;
